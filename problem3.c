59bad7581ee15217706c3c95ab7ac0436f23b6eb
akshay <akshay8055.007@gmail.com>
2022-04-05 18:12:33 +0100

    solved problem 3

--- problem3.c
+++ problem3.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
+//method used: fundamental theorem of arithmetic and sieve of Eratosthenes
 int main() {
     long number = 600851475143;
-    int max = 1;
-    int sqrt_number = (int)round(sqrt(number)); // 9
+    int sqrt_number = (int)round(sqrt(number));
     //printf("[!]Rounded value: %d\n", sqrt_number);
     int *prime_factor_decomp;
     prime_factor_decomp = (int *)malloc(100 * sizeof(int));
@@ -33,6 +33,11 @@ int main() {
         }
     }
 
+    printf("Prime factor decomposition: ");
+    for(int i = 0; i<sizeof *prime_factor_decomp; i++) {
+        printf("%d ", *(prime_factor_decomp + i));
+    }
+    printf("\n");
     printf("Max Prime Factor is: %d", *(prime_factor_decomp + sizeof *prime_factor_decomp-1));
 
     free(prime_factor_decomp);
@@ -40,25 +45,3 @@ int main() {
    printf("\n");
     return 0;
 }
-
-
-    // for(int i = 0; i<=sqrt_number; i++) {
-    //     if(number % number_array[i] == 0) {
-    //         printf("%d ! \n", number_array[i]);
-    //         while (1) {
-    //             int exp_count = 0;
-    //             if(number % (int)(pow(number_array[i], exp_count++)) == 0 && (number_array[i] != 0)) {
-    //                 printf("[][]%d\n" number_array[i]);
-    //                 number_array[i] = 0;
-    //             }
-    //             else {
-    //                 printf("%d", exp_count);
-    //                 number = number/pow(number_array[i], exp_count - 1);
-    //                 prime_factor_decomp[i] = number_array[i];
-                    
-    //                 break;
-    //             }
-    //         }
-            
-    //     }
-    
